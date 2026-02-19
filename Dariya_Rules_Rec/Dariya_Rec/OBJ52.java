54e0f5794601df1207b8144603f21c3e683a7504
DariyaKim <137013929+DariyaKim@users.noreply.github.com>
2026-02-18 18:10:31 -0600

    Update Dariya Rec

--- Dariya_Rules_Rec/Dariya_Rec/OBJ52.java
+++ Dariya_Rules_Rec/Dariya_Rec/OBJ52.java
@@ -1,5 +1,32 @@
+// OBJ52-J. Write garbage-collection-friendly code
+// implement short-lived objects for game messages
 package Dariya_Rec;
 
+// temporary game messages that is designed to be short-lived
+class GameMessage {
+    // immutable message content
+    private final String message;
+    // constructor initializes immutable data
+    GameMessage(String message) {
+        this.message = message;
+    }
+    // show message
+    void show() {
+        System.out.println(message);
+    }
+}
 public class OBJ52 {
-    
+    public static void main(String[] args){
+        // array of short lived event msg
+        String[] events = {
+            "Player collected a coin",
+            "Player found a key"
+        };
+        // create and discard GameMessage objects quickly
+        for (String event : events){
+            GameMessage msg = new GameMessage(event);
+            msg.show();
+        }
+        System.out.println("Round finished. All messages are temporary and discarded.");
+    }
 }
