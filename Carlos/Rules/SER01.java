9f49af3d70a0154d88b616a835c7f62b8c38edd2
carlosgothub <165727645+carlosgothub@users.noreply.github.com>
2026-02-19 21:10:48 -0600

    Update SER01.java

--- Carlos/Rules/SER01.java
+++ Carlos/Rules/SER01.java
@@ -45,7 +45,7 @@ public class SER01 {
          * @param out The ObjectOutputStream to write to
          * @throws IOException if an error occurs
          */
-        private void writeObject(ObjectOutputStream out) throws IOException {
+        private void writeObject(ObjectOutputStream out) throws IOException { //Follows Rec FIO53-J
             // Check player name
             if (playerName == null || playerName.trim().isEmpty()) {
                 playerName = "Player";
@@ -74,7 +74,7 @@ public class SER01 {
          * @throws IOException if an error occurs
          * @throws ClassNotFoundException if class cannot be found
          */
-        private void readObject(ObjectInputStream in)
+        private void readObject(ObjectInputStream in) //Follows Rec FIO53-J
                 throws IOException, ClassNotFoundException {
             // Default deserialization
             in.defaultReadObject();
@@ -135,35 +135,48 @@ public class SER01 {
          * Adds a joker card to active jokers
          *
          * @param jokerName The name of the joker card to add
+         * @return true if the joker was added (max 5 not reached), false
+         * otherwise
          */
-        public void addJoker(String jokerName) {
+        public boolean addJoker(String jokerName) {
             if (activeJokers.size() < 5) { // Max 5 jokers
                 activeJokers.add(jokerName);
                 System.out.println("Added joker: " + jokerName);
+                return true;
             }
+            System.out.println("Cannot add joker " + jokerName + " (limit reached)");
+            return false;
         }
 
         /**
          * Unlocks a tarot card
          *
          * @param tarotName The name of the tarot card to unlock
+         * @return true if the tarot was newly unlocked, false if already
+         * unlocked
          */
-        public void unlockTarot(String tarotName) {
+        public boolean unlockTarot(String tarotName) {
             if (!unlockedTarots.contains(tarotName)) {
                 unlockedTarots.add(tarotName);
+                System.out.println("Unlocked tarot: " + tarotName);
+                return true;
             }
+            System.out.println("Tarot " + tarotName + " already unlocked");
+            return false;
         }
 
         /**
-         * Updates chip total.
+         * Updates chip total
          *
          * @param chips The number of chips to add
+         * @return The new total chips (capped 0 if negative)
          */
-        public void updateChips(int chips) {
+        public int updateChips(int chips) {
             this.totalChips += chips;
             if (this.totalChips < 0) {
                 this.totalChips = 0;
             }
+            return this.totalChips;
         }
 
         /**
@@ -180,21 +193,38 @@ public class SER01 {
     /**
      * Code to show serialization
      */
+    @SuppressWarnings({"CallToPrintStackTrace", "UseSpecificCatch"})
     public static void main(String[] args) {
         // Create a game save
         BalatroSave save = new BalatroSave("Carlos");
 
         // Game progress
-        save.addJoker("Jolly Joker");
-        save.addJoker("Zany Joker");
-        save.unlockTarot("The Fool");
-        save.updateChips(2500);
+        System.out.println("Adding jokers:");
+        boolean added1 = save.addJoker("Jolly Joker");
+        boolean added2 = save.addJoker("Zany Joker");
+        boolean added3 = save.addJoker("Jolly Joker");
+        boolean added4 = save.addJoker("Sly Joker");
+        boolean added5 = save.addJoker("Crafty Joker");
+        boolean added6 = save.addJoker("Abstract Joker"); // should fail (max 5)
+        System.out.println("Add results: " + added1 + ", " + added2 + ", " + added3 + ", " + added4 + ", " + added5 + ", " + added6);
 
-        System.out.println("Current game state: " + save);
+        System.out.println("\nUnlocking tarots:");
+        boolean unlocked1 = save.unlockTarot("The Fool");
+        boolean unlocked2 = save.unlockTarot("The Magician");
+        boolean unlocked3 = save.unlockTarot("The Fool"); // already unlocked
+        System.out.println("Unlock results: " + unlocked1 + ", " + unlocked2 + ", " + unlocked3);
+
+        System.out.println("\nUpdating chips:");
+        int newTotal = save.updateChips(2500);
+        System.out.println("Chips after +2500: " + newTotal);
+        newTotal = save.updateChips(-3000);
+        System.out.println("Chips after -3000 (should cap at 0): " + newTotal);
+
+        System.out.println("\nCurrent game state: " + save);
+        File saveFile = new File("balatro_save.dat");
 
         try {
             // Save the game
-            File saveFile = new File("balatro_save.dat");
             try (FileOutputStream fileOut = new FileOutputStream(saveFile); ObjectOutputStream out = new ObjectOutputStream(fileOut)) {
                 out.writeObject(save);
             }
@@ -209,9 +239,15 @@ public class SER01 {
             System.out.println("Game loaded successfully!");
             System.out.println("Loaded state: " + loadedSave);
 
-            // Cleanup
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println("Error during serialization: " + e.getMessage());
+        } catch (Throwable t) {   // ERR53‑J: catch system errors
+            System.err.println("FATAL ERROR: " + t.getClass().getSimpleName() + " - " + t.getMessage());
+            t.printStackTrace();
+        } finally {
+            // Cleanup – runs even after a Throwable
             if (REMOVE_SAVE_FILE) {
-                if (saveFile.delete()) {
+                if (saveFile.exists() && saveFile.delete()) {
                     System.out.println("Cleanup: Save file deleted.");
                 } else {
                     System.out.println("Cleanup: Could not delete save file.");
@@ -219,9 +255,6 @@ public class SER01 {
             } else {
                 System.out.println("Cleanup: Save file preserved at: " + saveFile.getAbsolutePath());
             }
-
-        } catch (IOException | ClassNotFoundException e) {
-            System.err.println("Error during serialization: " + e.getMessage());
         }
     }
 }
