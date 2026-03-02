3e1b2f062a40e93856e6a9151db61ff350b23b3a
DariyaKim <137013929+DariyaKim@users.noreply.github.com>
2026-03-02 17:54:56 -0600

    Update Regular and VIP class

--- app/Regular.java
+++ app/Regular.java
@@ -23,6 +23,18 @@ class Regular {
 
     // package-private constructor for VIP promotion
     Regular(String playerName, int balance, int gamesPlayed, Date lastLogin){
+        if(playerName == null || playerName.isBlank()){
+            throw new IllegalArgumentException("Player name cannot be blank");
+        }
+        if(balance < 0){
+            throw new IllegalArgumentException("Balance cannot be negative!");
+        }
+        if(gamesPlayed <0){
+            throw new IllegalArgumentException("Games played cannot be negative!");
+        }
+        if(lastLogin == null){
+            throw new IllegalArgumentException("Last login cannot be null");
+        }
         this.playerName = playerName;
         this.balance = balance;
         this.gamesPlayed = gamesPlayed;
